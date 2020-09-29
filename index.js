7eda22a2561b165808d3d4f95a60228c95fb1a46
congphuong1606 <cphuong.coldboy@gmail.com>
2020-09-29 18:55:44 +0700

    add

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 var express = require('express')
+const axios = require('axios');
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
+app.get('/', function(req, res) {
+ let url = 'https://api-cloud.huobi.co.jp/market/detail/merged?symbol=btcjpy';
+ let price='0';
+  axios({
+      method:'get',
+      url
+  })
+  .then(function (response) {
+       res.send(JSON.stringify(response.data.tick.close));
+      console.log(response.data.tick.close);     
+  })
+  .catch(function (error) {
+      console.log(error);
+  });
 })
 
 app.listen(app.get('port'), function() {
